1adf067490211209e364fc9af24d9fab69813680
Tony DiCola <tony@tonydicola.com>
2015-07-07 11:32:07 -0700

    Improve status printing

--- install_openocd_debian.sh
+++ install_openocd_debian.sh
@@ -2,12 +2,16 @@
 set -e
 
 # First update the apt package list and install required dependencies.
-echo 'Installing dependencies (enter sudo password when prompted)...'
+echo '============================================================'
+echo 'Installing dependencies (enter sudo password if prompted)...'
+echo '============================================================'
 sudo apt-get update
 sudo apt-get install -y build-essential pkg-config libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev
 
 # Next download the source and unpack it (deleting any old version of it).
+echo '==================================='
 echo 'Downloading OpenOCD 0.9.0 source...'
+echo '==================================='
 if [ -f openocd-0.9.0.tar.gz ];
 then
   rm openocd-0.9.0.tar.gz
@@ -20,11 +24,15 @@ fi
 tar xvfz openocd-0.9.0.tar.gz
 
 # Finally change to openocd directory to build and install it.
+echo '========================================'
 echo 'Building and installing OpenOCD 0.9.0...'
+echo '========================================'
 cd openocd-0.9.0
 ./configure
 make
 sudo make install
 
+echo '====================================='
 echo 'Successfully installed OpenOCD 0.9.0!'
+echo '====================================='
 cd ..
